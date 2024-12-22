9e61a8fa1e607c10defed69788400833df18e629
Dakshatk <dakshat25k@gmail.com>
2024-12-22 21:54:45 +0530

    day19

---
+++ day19.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> findTargetIndices(const vector<int>& arr, int target) {
+    vector<int> sortedArr = arr;
+    sort(sortedArr.begin(), sortedArr.end());
+
+    vector<int> targetIndices;
+    for (size_t i = 0; i < sortedArr.size(); ++i) {
+        if (sortedArr[i] == target) {
+            targetIndices.push_back(i);
+        }
+    }
+
+    return targetIndices;
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, target;
+        cin >> N >> target;
+
+        vector<int> arr(N);
+        for (int i = 0; i < N; ++i) {
+            cin >> arr[i];
+        }
+
+        vector<int> result = findTargetIndices(arr, target);
+
+        if (result.empty()) {
+            cout << "" << endl;
+        } else {
+            for (size_t i = 0; i < result.size(); ++i) {
+                cout << result[i];
+                if (i != result.size() - 1) {
+                    cout << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
+2
+3 12 
+12 15 0 
+10 6 
+9 10 8 1 6 10 6 11 2 14
